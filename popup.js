79d912de74dbea2a8dc25fc955fab64ebbb3a184
Anna Gribtsova <adgribtsova@mai.education>
2022-12-06 22:11:53 +0300

    Создала основные файлы

---
+++ popup.js
@@ -0,0 +1,17 @@
+﻿let snow = document.getElementById("snow");
+// когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
+snow.addEventListener("click", async () => {
+    alert("Кролик номер ");
+    // получаем доступ к активной вкладке
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    // выполняем скрипт, если выполнен промис
+    chrome.scripting.executeScript({
+        // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
+        target: { tabId: tab.id, allFrames: true},
+        // вызываем функцию, в которой лежит запуск снежинок
+        function: snowFall
+    });
+});
+function snowFall() {
+   alert("Что-то я сделала!");
+}
