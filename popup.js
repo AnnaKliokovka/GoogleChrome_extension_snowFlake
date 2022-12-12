6b7dbf68448f673d9bb29272cbdf8cc27c184f9f
Anna Gribtsova <adgribtsova@mai.education>
2022-12-12 19:19:12 +0300

    Добавила выбор времени для таймера

--- popup.js
+++ popup.js
@@ -1,17 +1,23 @@
-﻿let snow = document.getElementById("snow");
-// когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
-snow.addEventListener("click", async () => {
-    alert("Кролик номер ");
-    // получаем доступ к активной вкладке
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    // выполняем скрипт, если выполнен промис
-    chrome.scripting.executeScript({
-        // скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
-        target: { tabId: tab.id, allFrames: true},
-        // вызываем функцию, в которой лежит запуск снежинок
-        function: snowFall
-    });
+﻿let minus = document.getElementById("MinusTime");
+let plus = document.getElementById("PlusTime");
+let time = document.getElementById("Time");
+
+minus.addEventListener("click", async () => {
+    minus_time();
 });
-function snowFall() {
-   alert("Что-то я сделала!");
+
+plus.addEventListener("click", async () => {
+    plus_time();
+});
+
+function minus_time() {
+    let t = time.textContent
+    if (t > 10)
+        time.textContent = t - 1;
+}
+
+function plus_time() {
+    let t = time.textContent
+    if (t < 90)
+        time.textContent = parseInt(t) + 1;
 }
