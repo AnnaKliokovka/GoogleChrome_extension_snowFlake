a5f39116980631db5450f5167289f221ffff9a40
Anna Gribtsova <adgribtsova@mai.education>
2022-12-13 11:50:39 +0300

    Добавила таймер + кнопки
    Кнопки: старт, стоп и сброс

--- popup.js
+++ popup.js
@@ -1,23 +1,77 @@
 ﻿let minus = document.getElementById("MinusTime");
 let plus = document.getElementById("PlusTime");
-let time = document.getElementById("Time");
+let T_minutes = document.getElementById("Minutes");
+let T_seconds = document.getElementById("Seconds");
+let start = document.getElementById("St-btn");
+let stop = document.getElementById("Sp-btn");
+let restart = document.getElementById("Re-btn");
+let IsStart = false;
+let StartMinutes = 30;
 
 minus.addEventListener("click", async () => {
-    minus_time();
+    minus_time(5);
 });
 
 plus.addEventListener("click", async () => {
     plus_time();
 });
 
-function minus_time() {
-    let t = time.textContent
-    if (t > 10)
-        time.textContent = t - 1;
+start.addEventListener("click", async()=>{
+  start_time();
+})
+stop.addEventListener("click", async()=>{
+  stop_time();
+})
+restart.addEventListener("click", async()=>{
+  restart_time();
+})
+
+function minus_time(min_t){
+  let t = T_minutes.textContent
+  if (t>min_t)
+     T_minutes.textContent = t - 1;
 }
 
-function plus_time() {
-    let t = time.textContent
-    if (t < 90)
-        time.textContent = parseInt(t) + 1;
+function plus_time(){
+  let t = T_minutes.textContent
+  if (t< 90)
+    T_minutes.textContent = parseInt(t) + 1;
 }
+
+function start_time(){
+  StartMinures = T_minutes.textContent;
+  IsStart = true;
+  
+}
+
+function stop_time(){
+  IsStart = false;
+}
+function restart_time(){
+  IsStart = false;
+  T_minutes.textContent = StartMinures;
+  T_seconds.textContent = pad(0);
+}
+
+function minus_seconds(){
+  let s = T_seconds.textContent;
+  if (s == 00){
+    T_seconds.textContent = 59;
+    minus_time(0);
+  }
+  else{
+    T_seconds.textContent = pad(s-1);
+  }  
+}
+
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
+timer = setInterval(function(){
+  if (IsStart){
+  minus_seconds()
+  seconds = T_seconds.textContent;
+  minutes = T_minutes.textContent;
+  }
+}, 1000)
